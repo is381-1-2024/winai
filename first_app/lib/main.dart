bd5d04e4c67292ff2e0831a4603ef76c9a8dfe83
Winai Nadee <winain@gmail.com>
2024-08-30 16:30:57 +0700

    Week02

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -12,10 +12,15 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.brown),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/1',
+      routes: {
+        '/1': (context) => FirstPage(),
+        '/2': (context) => SecondPage(),
+        '/3': (context) => ThirdPage(),
+      },
     );
   }
 }
@@ -54,6 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
+            NameCard(),
+            NameCard(),
             NameCard(),
             const Text(
               'You have pushed the button this many times:',
@@ -98,14 +105,16 @@ class NameCard extends StatelessWidget {
       margin: EdgeInsets.all(8.0),
       padding: EdgeInsets.all(8.0),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           SizedBox(
-            height: 200.0,
+            height: 150.0,
             child: Image.asset(
               'assets/images/AvatarMe.jpg',
             ),
           ),
           Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text('Name: Winai Nadee'),
               Text('DOB: xxx Month 2000'),
@@ -117,3 +126,125 @@ class NameCard extends StatelessWidget {
   }
 }
 
+class FirstPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('First Page'),
+      ),
+      body: GridView.count(
+        crossAxisCount: 2,
+        children: List.generate(3, (index) {
+          return InkWell(
+            onTap: () {
+              if (index == 0) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text('Page 1 is alreay here'),
+                  ),
+                );
+                return;
+              }
+
+              Navigator.pushNamed(context, '/${index + 1}');
+            },
+            child: Container(
+              margin: EdgeInsets.all(8.0),
+              padding: EdgeInsets.all(8.0),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.inversePrimary,
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.home),
+                  Text('Page ${index + 1}'),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  final List<String> entries = <String>['A', 'B', 'C', 'D', 'E', 'F', 'G'];
+  final List<int> colorCodes = <int>[600, 500, 100, 600, 500, 100, 600];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text('Second Page'),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(8.0),
+        itemCount: entries.length,
+        separatorBuilder: (context, index) {
+          return Divider();
+      },
+        itemBuilder: (context, index) {
+          return Container(
+            height: 250,
+            color: Colors.amber[colorCodes[index]],
+            child: Center(
+              child: Column(
+                children: [
+                  Text('Item ${entries[index]}'),
+                  NameCard(),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text('Third Page'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add_alert),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: Icon(Icons.home),
+            onPressed: () {
+              Navigator.pushNamed(context, '/1');
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Show msg'),
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(
+                content: Text('Hello'),
+                action: SnackBarAction(
+                  label: 'Okay',
+                  onPressed: () {},
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
